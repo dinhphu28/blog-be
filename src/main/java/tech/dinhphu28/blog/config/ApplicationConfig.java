acf4d3fde1bc46742877396d021711cc18dd2c00
Anthony <18110175@student.hcmute.edu.vn>
2023-07-26 21:30:31 +0700

    Add Bean UserDetailsService

--- src/main/java/tech/dinhphu28/blog/config/ApplicationConfig.java
+++ src/main/java/tech/dinhphu28/blog/config/ApplicationConfig.java
@@ -3,12 +3,23 @@ package tech.dinhphu28.blog.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import tech.dinhphu28.blog.repository.UserRepository;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
