ce958cf70ce6f2b8b965815424bd6746e75231cb
Anthony <18110175@student.hcmute.edu.vn>
2023-07-26 20:45:02 +0700

    Add Bean PasswordEncoder

--- src/main/java/tech/dinhphu28/blog/config/ApplicationConfig.java
+++ src/main/java/tech/dinhphu28/blog/config/ApplicationConfig.java
@@ -1,11 +1,17 @@
 package tech.dinhphu28.blog.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
