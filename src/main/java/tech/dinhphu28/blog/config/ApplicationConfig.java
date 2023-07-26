e960eb62a9f15294f8f465617c465993f5f3f43d
Anthony <18110175@student.hcmute.edu.vn>
2023-07-26 13:12:19 +0700

    Add ApplicationConfig.java

---
+++ src/main/java/tech/dinhphu28/blog/config/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package tech.dinhphu28.blog.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+
+}
