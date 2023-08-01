7ddc1a3425e0f64f6932035da7c5af4b59c57eff
Anthony <18110175@student.hcmute.edu.vn>
2023-08-01 08:54:11 +0700

    Update Verification Request & Response models
    Add fields

--- src/main/java/tech/dinhphu28/blog/model/VerificationResponse.java
+++ src/main/java/tech/dinhphu28/blog/model/VerificationResponse.java
@@ -1,4 +1,13 @@
 package tech.dinhphu28.blog.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class VerificationResponse {
+    private Integer status;
+    private String message;
 }
