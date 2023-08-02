551116d92e548965e6d3e54e81ba08d98aa5c4c8
Anthony <18110175@student.hcmute.edu.vn>
2023-08-02 14:23:28 +0700

    Update AuthenticationController - Add verifing otp to activate user

--- src/main/java/tech/dinhphu28/blog/controller/AuthenticationController.java
+++ src/main/java/tech/dinhphu28/blog/controller/AuthenticationController.java
@@ -1,14 +1,14 @@
 package tech.dinhphu28.blog.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tech.dinhphu28.blog.model.AuthenticationResponse;
-import tech.dinhphu28.blog.model.RegisterRequest;
-import tech.dinhphu28.blog.model.RegisterResponse;
+import tech.dinhphu28.blog.exception.OTPNotVerifiedException;
+import tech.dinhphu28.blog.model.*;
 import tech.dinhphu28.blog.service.AuthenticationService;
 
 @RestController
@@ -19,9 +19,21 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping(
-            value = "/register"
+            value = "/register",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping(
+            value = "/verify",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<VerificationResponse> verify(@RequestBody VerificationRequest request) {
+
+        return ResponseEntity.ok(authenticationService.verifyAndActivateUser(request));
     }
 }
