0aa9e93c51616590457670c97576da1b366e792d
Anthony <18110175@student.hcmute.edu.vn>
2023-08-03 22:23:40 +0700

    Update API Handle Exception - Add handleUsernameNotFoundException

--- src/main/java/tech/dinhphu28/blog/config/ApplicationExceptionHandler.java
+++ src/main/java/tech/dinhphu28/blog/config/ApplicationExceptionHandler.java
@@ -2,6 +2,7 @@ package tech.dinhphu28.blog.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,16 +22,33 @@ public class ApplicationExceptionHandler {
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(OTPNotVerifiedException.class)
-    public ExceptionHandlerResponse handleOTPNotVerifiedException(OTPNotVerifiedException exception, HttpServletRequest request) {
+    public ExceptionHandlerResponse handleOTPNotVerifiedException(OTPNotVerifiedException exception,
+                                                                  HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.UNAUTHORIZED;
 
         ExceptionHandlerResponse exceptionHandlerResponse = ExceptionHandlerResponse.builder()
                 .timestamp(sdf.format(new Date()))
-                .status(HttpStatus.UNAUTHORIZED.value())
-                .error(HttpStatus.UNAUTHORIZED.name())
+                .status(httpStatus.value())
+                .error(httpStatus.getReasonPhrase())
                 .message(exception.getMessage())
                 .path(request.getServletPath())
                 .build();
 
+        return exceptionHandlerResponse;
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ExceptionHandlerResponse handleUsernameNotFoundException(UsernameNotFoundException exception,
+                                                                    HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+
+        ExceptionHandlerResponse exceptionHandlerResponse = ExceptionHandlerResponse.builder()
+                .timestamp(sdf.format(new Date()))
+                .status(httpStatus.value())
+                .error(httpStatus.getReasonPhrase())
+                .message(exception.getMessage())
+                .path(request.getServletPath())
+                .build();
         return exceptionHandlerResponse;
     }
 }
