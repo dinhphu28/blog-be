85dd058c0e3f2cd462a8c6e1ad8a85a53a9339f1
Anthony <18110175@student.hcmute.edu.vn>
2023-08-06 19:34:10 +0700

    Remove ResponseStatus OTPNotVerifiedException

--- src/main/java/tech/dinhphu28/blog/exception/OTPNotVerifiedException.java
+++ src/main/java/tech/dinhphu28/blog/exception/OTPNotVerifiedException.java
@@ -3,7 +3,6 @@ package tech.dinhphu28.blog.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class OTPNotVerifiedException extends RuntimeException {
     public OTPNotVerifiedException(String msg) {
         super(msg);
